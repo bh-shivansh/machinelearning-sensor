00b4bad62a59d8f5f00bc33e53a2d598edad6250
bh-shivansh <shbhardwaj020@gmail.com>
2024-07-16 09:25:08 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -22,7 +22,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+def evaluate_models(xtrain, ytrain,xtest,ytest,models,param):
     try:
         report = {}
 
@@ -31,20 +31,20 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             para=param[list(models.keys())[i]]
 
             gs = GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
+            gs.fit(xtrain,ytrain)
 
             model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
+            model.fit(xtrain,ytrain)
 
             #model.fit(X_train, y_train)  # Train model
 
-            y_train_pred = model.predict(X_train)
+            y_train_pred = model.predict(xtrain)
 
-            y_test_pred = model.predict(X_test)
+            y_test_pred = model.predict(xtest)
 
-            train_model_score = r2_score(y_train, y_train_pred)
+            train_model_score = r2_score(ytrain, y_train_pred)
 
-            test_model_score = r2_score(y_test, y_test_pred)
+            test_model_score = r2_score(ytest, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
 
